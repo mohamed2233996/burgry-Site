a1679321308b23a26998688a34c69e212b9e5673
mohamed2233996 <medomano771@gmail.com>
2024-05-07 11:31:13 +0300

    v2.0.11

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,20 +8,20 @@ module.exports = {
   ],
   theme: {
     extend: {
-      // backgroundImage: {
-      //   "burger-intro":"url('/download1.png')",
-      //   "snack-intro":"url('/download2.png')",
-      //   "drink-intro":"url('/download3.png')",
-      //   "offerbg": "url('/offer.jpg')",
-      // },
-      colors: {
-        primary: "#ffba00",
-        black: "#000000",
-        boxColor:"#171717",
-        opacityColor: "rgba(0, 0, 0, 0.75);",
-        opacityColorLinearStart: "rgba(0,0,0,0.75) 50%;",
-        opacityColorLinearEnd: "rgba(0,0,0,1) 100%;",
-      }
+      backgroundImage: {
+        "burger-intro":"url('/download1.png')",
+        "snack-intro":"url('/download2.png')",
+        "drink-intro":"url('/download3.png')",
+        "offerbg": "url('/offer.jpg')",
+      },
+      // colors: {
+      //   primary: "#ffba00",
+      //   black: "#000000",
+      //   boxColor:"#171717",
+      //   opacityColor: "rgba(0, 0, 0, 0.75);",
+      //   opacityColorLinearStart: "rgba(0,0,0,0.75) 50%;",
+      //   opacityColorLinearEnd: "rgba(0,0,0,1) 100%;",
+      // }
     },
   },
   plugins: [daisyui],
